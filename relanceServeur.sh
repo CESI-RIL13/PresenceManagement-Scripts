69dbb4d913984aaa46b6e59d57a1b766fed60462
cedric moizeau <cedric@debian>
2015-03-10 14:08:16 +0100

    modification du script relanceServeur pour le passer en cron

--- relanceServeur.sh
+++ relanceServeur.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 # chemin du serveur
 process=$1
-if [ -e $process ]; then
-	while [ 1 = 1 ]; do
-		# recherche parmis les processus en cours le process en excluant la chaine "grep"
-		ps ax | grep $process | grep -v grep | grep -v $0
-		# si il y a un résultat
-		if [ $? -eq 0 ]; then
-			echo "le script est actuellement en train de tourner"
-		else 
-			/etc/init.d/monserveur restart
-		fi
-		sleep 5
-	done
+if [ -e $process ]; then	
+	# recherche parmis les processus en cours le process en excluant la chaine "grep"
+	ps ax | grep $process | grep -v grep | grep -v $0
+	# si il y a un résultat
+	if [ $? -eq 0 ]; then
+		echo "le script est actuellement en train de tourner"
+	else 
+		/etc/init.d/monserveur restart
+	fi
 else
 	echo $1 + "n'existe pas"
 fi
