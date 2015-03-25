3da0c0f4666bb9bb4d85793e0d864f53710e4141
cedric moizeau <cedric@debian>
2015-03-25 17:03:35 +0100

    finalisation import planning

--- import/Importation.py
+++ import/Importation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: latin-1 -*-
 __author__ = 'cedric'
-import urllib2, jsonpickle, copy
+import urllib2, jsonpickle, copy, urllib
+from datetime import datetime
+from  time import mktime
 
 class Importation:
     def __init__(self, adresseServeur):
@@ -12,11 +14,68 @@ class Importation:
         if "user.id" in self.data[0]:
             self.importUsers(self.data)
         elif "promotion.id" in self.data[0]:
-            print "import promotion"
-        elif "scheduling.*" in self.data[0]:
-            print "import scheduling"
+            print("import promotion")
+        elif "scheduling.date" in self.data[0]:
+            self.importSchedulings(self.data)
         else:
-            print "import room"
+            print("import room")
+
+    def isPresent(self,tab,name):
+        for liste in tab:
+            if liste["name"] == name:
+                return True
+        return False
+
+    def importSchedulings(self, liste):
+        tabRooms=[]
+        tabRoomsExistantes=[]
+        for scheduling in liste:
+            if not self.isPresent(tabRooms, scheduling["scheduling.room_id"]) and not self.isPresent(tabRoomsExistantes,scheduling["scheduling.room_id"]):
+                url=self.adresseServeur+"rooms/?"+ urllib.urlencode({"name" : scheduling["scheduling.room_id"]})
+                try:
+                    room=urllib2.urlopen(url)
+                    tabRoomsExistantes.append({"name" : scheduling["scheduling.room_id"], "add" : False})
+                except urllib2.HTTPError as e:
+                    if e.getcode() == 404:
+                        tabRooms.append({"name" : scheduling["scheduling.room_id"]})
+                    pass
+        print(tabRooms)
+        self.ajouterRooms(tabRooms)
+
+        schedulings=[]
+        rooms={}
+        users={}
+        for planning in liste:
+            # planning={}
+            scheduling={}
+            planning["scheduling.date"]=str(planning["scheduling.date"]).split('. ')[1]
+            # print planning["scheduling.date"]
+            heures=str(planning["scheduling.heure"]).split(" / ")
+            # print heures
+
+            from_dateStart=planning["scheduling.date"]+ " " + heures[0]
+            scheduling["date_start"] = str(mktime(datetime.strptime(from_dateStart,"%d/%m/%y %H:%M").timetuple()))
+            from_dateEnd=planning["scheduling.date"] + " " + heures[1]
+            scheduling["date_end"]= str(mktime(datetime.strptime(from_dateEnd,"%d/%m/%y %H:%M").timetuple()))
+
+            if not planning["scheduling.room_id"] in rooms:
+                rooms[planning["scheduling.room_id"]]=self.obtenirIdRoom(planning["scheduling.room_id"])
+            scheduling["room_id"]=str(rooms[planning["scheduling.room_id"]])
+
+            if not planning["scheduling.user_id"] in users:
+                users[planning["scheduling.user_id"]]=self.obtenirIdUser(planning["scheduling.user_id"])
+            scheduling["user_id"]=users[planning["scheduling.user_id"]]
+
+            scheduling["promotion_id"]=planning["scheduling.promotion_id"]
+
+            if planning.has_key("scheduling.course"):
+                scheduling["course"] = planning["scheduling.course"]
+
+            schedulings.append(scheduling)
+
+        print(schedulings)
+        self.ajouterSchedulings(schedulings)
+        return
 
     def importUsers(self, liste):
         tabPromo=[]
@@ -25,38 +84,68 @@ class Importation:
             req = urllib2.Request(url)
             try:
                 promo=urllib2.urlopen(req)
-                print promo.read()
+                # print(promo.read())
             except urllib2.HTTPError as e:
                 if e.getcode() == 404:
                     name=str(utilisateur["promotion.name"]).split(" ")
-                    print name
+                    # print(name)
                     tabPromo.append({"id" : utilisateur["user.promotion_id"], "name" : name[0] + name[1]})
-                    print tabPromo
+                    # print(tabPromo)
                 pass
         self.ajouterPromotions(tabPromo)
 
         users=[]
         for utilisateur in liste:
+            # utilisateur={}
             user={}
-            print utilisateur
+            # print(utilisateur)
+            utilisateur["user.fullname"] = utilisateur["user.firstname"] + " " + utilisateur["user.name"]
+            utilisateur["user.role"]="stagiaire"
+            del utilisateur["user.name"]
+            del utilisateur["user.firstname"]
+            # print(utilisateur)
             for clef,valeur in utilisateur.items():
                 key=str(clef).split('.')
                 if key[0] == "user":
                     user[key[1]]=valeur
+            # print user
             users.append(user)
         self.ajouterUsers(users)
         return
 
+    def obtenirIdUser(self, nomUser):
+        url=self.adresseServeur +"users/?"
+        data=urllib.urlencode({"fullname":nomUser})
+        try:
+            resp=urllib2.urlopen(url+data)
+            return jsonpickle.decode(resp.read())[0]["id"]
+        except urllib2.HTTPError as e:
+            if e.getcode() == 404:
+                return ""
+            pass
+
+    def obtenirIdRoom(self, nomRoom):
+        url=self.adresseServeur+"rooms/?"
+        data=urllib.urlencode({"name":nomRoom})
+        resp=urllib2.urlopen(url + data)
+        return jsonpickle.decode(resp.read())[0]["id"]
+
     def ajouterTableauDonnees(self,tableau,url):
         req=urllib2.Request(url,tableau,{"content-type":"application/json"})
         resp=urllib2.urlopen(req)
-        print resp.read()
+        print(resp.read())
 
     def ajouterUsers(self,users):
         self.ajouterTableauDonnees(jsonpickle.encode(users),self.adresseServeur+"users/")
 
     def ajouterPromotions(self, promos):
         self.ajouterTableauDonnees(jsonpickle.encode(promos),self.adresseServeur+"promotions/")
+
+    def ajouterRooms(self, rooms):
+        self.ajouterTableauDonnees(jsonpickle.encode(rooms),self.adresseServeur+"rooms/")
+
+    def ajouterSchedulings(self, schedulings):
+        self.ajouterTableauDonnees(jsonpickle.encode(schedulings), self.adresseServeur+"schedulings/")
 
     def ajouterDonnees(self,ligne):
         self.data.append(copy.copy(ligne))
--- import/lectureExport.py
+++ import/lectureExport.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import __future__
 __author__ = 'cedric'
 import configparser
 import csv
+import unicodedata
 from Importation import Importation
 from sys import argv
+
+def remove_accents(input_str):
+    nkfd_form = unicodedata.normalize('NFKD', unicode(input_str))
+    return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
 
 def creerConfig(configuration, prompt, champs, section="nomChamps"):
     configuration.set(section, champs, raw_input(prompt))
@@ -19,6 +25,7 @@ def getNomConfig(config, value, section = "nomChamps"):
 def mappageConfig(nomConfig, map):
     return map[nomConfig] if nomConfig in map else -1
 
+#print open("lectureExport.cfg","r")
 configuration=configparser.RawConfigParser()
 configuration.read("lectureExport.cfg")
 complete= configuration.has_section("nomChamps") and configuration.has_section("autres")
@@ -36,11 +43,12 @@ if not complete:
         "de l'identifiant d'une promotion : " : 'promotion.id',
         "du nom de la promotion : " : 'promotion.name',
         "du nom de la salle : " : 'room.name',
-        "de la date de début de l'entrée du planning : " : 'scheduling.date_start',
-        "de la date de fin de l'entrée du planning : " : 'scheduling.date_end',
+        "de la date l'entrée du planning : " : 'scheduling.date',
+        "de l'heure de l'entrée du planning : " : 'scheduling.heure',
         "de la salle de l'entrée du planning : " : 'scheduling.room_id',
         "de la promo pour l'entrée du planning : " : 'scheduling.promotion_id',
         "de l'identifiant de l'intervenant de l'entrée du planning : " : 'scheduling.user_id',
+        "de la matière de l'entrée du planning : " : 'scheduling.course',
         "Entrer l'adresse du serveur : " : "adresseserveur"
     }
 
@@ -57,8 +65,8 @@ if not complete:
 URLSERVEUR=configuration.get("autres","adresseserveur")
 
 nomFichier=""
-if __debug__:
-    nomFichier="structure export AD.csv"
+if len(argv) == 1:
+    nomFichier="seancesDebut.csv"
 else:
     nomFichier=argv[1]
 
@@ -66,31 +74,40 @@ cr = csv.reader(open(nomFichier,"rb"))
 map={}
 pos={}
 champsImportes={}
+promo=0
 importation = Importation(configuration.get("autres","adresseserveur"))
 for row in cr:
     if pos == {}:
-        index=0
-        for nomChamps in row:
-            if getNomConfig(configuration,nomChamps) <> None:
-                map[getNomConfig(configuration,nomChamps)]=index
-            index+=1
-        pos["user.id"]=mappageConfig("user.id",map)
-        pos["user.name"]=mappageConfig("user.name",map)
-        pos["user.firstname"]= mappageConfig("user.firstname",map)
-        pos["user.mail"]= mappageConfig("user.mail",map)
-        pos["user.promotion_id"]=mappageConfig("user.promotion_id",map)
-        pos["promotion.id"]=mappageConfig("promotion.id",map)
-        pos["promotion.name"]= mappageConfig("promotion.name",map)
-        pos["room.name"]=mappageConfig("room.name",map)
-        pos["scheduling.date_start"] =mappageConfig("scheduling.date_start",map)
-        pos["scheduling.date_end"]=mappageConfig("scheduling.date_end",map)
-        pos["scheduling.room_id"]=mappageConfig("scheduling.room_id",map)
-        pos["scheduling.promotion_id"]=mappageConfig("scheduling.promotion_id",map)
-        pos["scheduling.user_id"]=mappageConfig("scheduling.user_id",map)
+        if len(row) > 1:
+            index=0
+            for nomChamps in row:
+                nomChamps = remove_accents(nomChamps.decode("utf-8"))
+                if getNomConfig(configuration,nomChamps) <> None:
+                    map[getNomConfig(configuration,nomChamps)]=index
+                index+=1
+            pos["user.id"]=mappageConfig("user.id",map)
+            pos["user.name"]=mappageConfig("user.name",map)
+            pos["user.firstname"]= mappageConfig("user.firstname",map)
+            pos["user.mail"]= mappageConfig("user.mail",map)
+            pos["user.promotion_id"]=mappageConfig("user.promotion_id",map)
+            pos["promotion.id"]=mappageConfig("promotion.id",map)
+            pos["promotion.name"]= mappageConfig("promotion.name",map)
+            pos["room.name"]=mappageConfig("room.name",map)
+            pos["scheduling.date"] =mappageConfig("scheduling.date",map)
+            pos["scheduling.heure"]=mappageConfig("scheduling.heure",map)
+            pos["scheduling.room_id"]=mappageConfig("scheduling.room_id",map)
+            pos["scheduling.promotion_id"]=mappageConfig("scheduling.promotion_id",map)
+            pos["scheduling.user_id"]=mappageConfig("scheduling.user_id",map)
+            pos["scheduling.course"]=mappageConfig("scheduling.course", map)
+        else:
+            promo=row[0]
     else:
         for clef,valeur in pos.items():
             if valeur <> -1:
-                champsImportes[clef]="%s"%(row[valeur])
+                champsImportes[clef]="%s"%(remove_accents(row[valeur].decode("utf-8")))
+        if promo <> 0:
+            champsImportes["scheduling.promotion_id"]=promo
+        # print champsImportes
         importation.ajouterDonnees(champsImportes)
         champsImportes.clear()
 
